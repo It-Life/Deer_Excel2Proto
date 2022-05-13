c26a32bc8d963b646d08aa95ddef92fa73997e73
it_Life <652597991@qq.com>
2022-05-13 18:50:49 +0800

    简化生成表复制到对应路径

--- MarkConfigAndProtobuf/BuildConfig_100.bat
+++ MarkConfigAndProtobuf/BuildConfig_100.bat
@@ -1,9 +1,11 @@
 @echo 复制到100
 
 cd /d %~dp0
+set WORK_NUMBER=100
 set ROOT_PATH=%~dp0
 set DATA_OUTPUT=%ROOT_PATH%client\data
-set DATA_UPLOAD_OUTPUT=%ROOT_PATH%..\..\xGame\GameFramework200730\CommitResources\100\Windows\Config
+set GAME_COMMIT_PATH=%ROOT_PATH%..\..\Deer_Gameframework_ToLua\CommitResources
+set DATA_UPLOAD_OUTPUT=%GAME_COMMIT_PATH%\%WORK_NUMBER%\Windows\Config
 
 call BuildConfig.bat
 echo ======== 开始复制Data文件 ========
@@ -11,8 +13,8 @@ xcopy /s/y/i "%DATA_OUTPUT%\*.bin" "%DATA_UPLOAD_OUTPUT%"
 echo ======== 复制Data文件结束 ========
 
 echo ======== 开始生成版本文件 ========
-set configPath=D:\Working\DeerGameFramework\card\xGame\GameFramework200730\CommitResources\100\Windows\Config
-set configVersionOutPath=D:\Working\DeerGameFramework\card\xGame\GameFramework200730\CommitResources\100\Windows
+set configPath=%GAME_COMMIT_PATH%\%WORK_NUMBER%\Windows\Config
+set configVersionOutPath=%GAME_COMMIT_PATH%\%WORK_NUMBER%\Windows
 call ConfigVersion.exe %configPath% %configVersionOutPath%
 echo ======== 生成版本文件结束 ========
 pause
